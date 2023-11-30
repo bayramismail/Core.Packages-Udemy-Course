78e0d2158a0e04eb1bf19be38b9455f86d505d49
bayramismail <83179561+bayramismail@users.noreply.github.com>
2023-11-30 23:42:41 +0300

    Mysql Logger added

---
+++ Core.CrossCuttingConcerns/Serilog/ConfigurationModels/MySqlLogConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+
+public class MySqlLogConfiguration
+{
+    public string ConnectionString { get; set; }
+    public string TableName { get; set; }
+    public bool StoreTimestampInUtc { get; set; }
+    public MySqlLogConfiguration()
+    {
+        ConnectionString = string.Empty;
+
+        TableName = string.Empty;
+
+    }
+
+    public MySqlLogConfiguration(string connectionString, string tableName, bool storeTimestampInUtc)
+    {
+        ConnectionString = connectionString;
+        TableName = tableName;
+        StoreTimestampInUtc = storeTimestampInUtc;
+    }
+}
---
+++ Core.CrossCuttingConcerns/Serilog/Loggers/MySqlLogger.cs
@@ -0,0 +1,27 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+
+namespace Core.CrossCuttingConcerns.Serilog.Loggers;
+
+public class MySqlLogger:LoggerServiceBase
+{
+    private readonly IConfiguration _configuration;
+
+    public MySqlLogger(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        MySqlLogConfiguration loggeConfiguration = _configuration.GetSection("SerilLogConfigurations:MysqlConfiguration")
+          .Get<MySqlLogConfiguration>() ?? throw new Exception(SeriLogMessages.NullOptionsMessage);
+        Logger seriLogConfig = new LoggerConfiguration()
+            .WriteTo.MySQL(
+            loggeConfiguration.ConnectionString,
+            tableName:loggeConfiguration.TableName,
+            storeTimestampInUtc:loggeConfiguration.StoreTimestampInUtc
+            )
+            .CreateLogger();
+        Logger = seriLogConfig;
+    }
+}
